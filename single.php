839603401ef6fea108195f0c55f45d3593e5b575
bosifullstack <bosifullstack@gmail.com>
2021-06-20 14:05:48 -0300

    Remove details section from post

--- single.php
+++ single.php
@@ -4,10 +4,6 @@ get_header();
 wp_enqueue_style('single');
 
 $hero = get_field('foto_hero');
-$overview = get_field('overview');
-$ano = get_field('ano');
-$plataforma = get_field('plataforma');
-$papel = get_field('papel');
 ?>
 <?php if (get_the_post_thumbnail() !== '') : ?>
 
@@ -19,31 +15,9 @@ $papel = get_field('papel');
 
 <div class="content">
   <h1 class="single-title"><?= get_the_title(); ?></h1>
-
-  <div class="single-flex">
-    <div class="single-overview mobile-hide">
-      <h1 class="single-overview-title">Overview</h1>
-      <p class="single-overview-text"><?= $overview; ?></p>
-    </div>
-    <div class="single-details">
-      <div class="single-details-content">
-        <h3 class="single-details-title">Ano</h3>
-        <p class="single-details-text"><?= $ano; ?></p>
-      </div>
-      <div class="single-details-content">
-        <h3 class="single-details-title">Plataforma</h3>
-        <p class="single-details-text"><?= $plataforma; ?></p>
-      </div>
-      <div class="single-details-content mobile-hide">
-        <h3 class="single-details-title">Papel</h3>
-        <p class="single-details-text"><?= $papel; ?></p>
-      </div>
-    </div>
-  </div>
 </div>
 
 <div class="single-content">
-  <h1><?= get_the_title(); ?></h1>
   <?php the_content(); ?>
 </div>
 
